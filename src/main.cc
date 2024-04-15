b9908d51a17ccc976282089668450f7d4e05e992
Felipe <felipevalerioderamos@gmail.com>
2024-04-14 21:52:45 -0300

    working with the first issue

--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,24 @@ using namespace std;
 
 int main() {
 	// just a sentence to start, later on it will be a text file
-	auto sentence = "o rato roeu a roupa do rei de roma";
+	string sentence = "o rato roeu a roupa do rei de roma";
+	auto count = 0;
 
-	
+	// string to array of char
+	auto length = sentence.length();
+	char* char_sentence = new char[length + 1];
+
+	//copying the content of sentence to char_sentence
+	strcpy(char_sentence, sentence.c_str());
+
+	for (auto i = 0; i < length; i++) {
+		for (auto j = 0; j < length; j++)
+			if (char_sentence[i] == char_sentence[j]) {
+				std::cout << "Encontrei uma segunda ocorrencia" << std::endl;
+			} else {
+				std::cout << "Somente uma ocorrencia" << std::endl;
+			}
+	}
 
 	return 0;
 }
